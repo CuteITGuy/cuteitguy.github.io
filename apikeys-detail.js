943d1253019d8ded08d3681b90aab2a89a6ee8a1
Apple <apple@Apples-iMac.lan>
2017-12-24 15:04:15 +0700

    Finish apikey-detail page

---
+++ apikeys-detail.js
@@ -0,0 +1,48 @@
+;
+
+(() => {
+  $(() => {
+    const $exchangeMarkets = $('#exchange-markets');
+    const $exchangeMarket = $('.exchange-market');
+
+    $exchangeMarkets.owlCarousel({
+      loop: false,
+      rewind: true,
+      margin: 80,
+      nav: false,
+      autoWidth: true,
+    });
+
+    $exchangeMarket.click(function ($event) {
+      const $this = $(this);
+      const exchangeName = $this.data('exchange-name');
+      const exchangeId = $this.data('exchange-id');
+      const linkHowto = $this.data('link-howto');
+
+      $exchangeMarket.removeClass('active');
+      $this.addClass('active');
+
+      setExchangeName(exchangeName);
+      setExchangeId(exchangeId);
+      setLinkHowto(exchangeName, linkHowto);
+    });
+  });
+
+
+  function setExchangeName(exchangeName) {
+    $('#lbKey').text(`${exchangeName} API Key`);
+    $('#lbSecret').text(`${exchangeName} API Secret`);
+    $('#inpKey').attr('placeholder', `Your ${exchangeName} API Key`);
+    $('#inpSecret').attr('placeholder', `Your ${exchangeName} API Key Secret`);
+  }
+
+
+  function setExchangeId(exchangeId) {
+    $('#inpExchangeId').val(exchangeId.toString());
+  }
+
+
+  function setLinkHowto(exchangeName, linkHowto) {
+    $('#lnkHowTo').attr('href', linkHowto.toString()).text(`How to get your API key and API secret from ${exchangeName}`);
+  }
+})();
